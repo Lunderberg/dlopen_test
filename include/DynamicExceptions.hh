048d0e463b0633ab0c6fc6a602faafe03c0e8589
Eric Lunderberg <lunderberg@nscl.msu.edu>
2015-10-11 15:34:09 -0400

    Compatability for g++ 4.7

--- include/DynamicExceptions.hh
+++ include/DynamicExceptions.hh
@@ -4,19 +4,19 @@
 #include <stdexcept>
 
 struct DynamicException : public std::runtime_error {
-  using std::runtime_error::runtime_error;
+  DynamicException(const std::string& msg) : std::runtime_error(msg) { }
 };
 
 struct DynamicFileNotFound : public DynamicException {
-  using DynamicException::DynamicException;
+  DynamicFileNotFound(const std::string& msg) : DynamicException(msg) { }
 };
 
 struct DynamicSymbolNotFound : public DynamicException {
-  using DynamicException::DynamicException;
+  DynamicSymbolNotFound(const std::string& msg) : DynamicException(msg) { }
 };
 
 struct DynamicSymlinkCreation : public DynamicException {
-  using DynamicException::DynamicException;
+  DynamicSymlinkCreation(const std::string& msg) : DynamicException(msg) { }
 };
 
 #endif /* _DYNAMICEXCEPTIONS_H_ */
